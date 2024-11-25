5f9e0c01d7647d7381bb5177c3509e1159be05af
김문규 <moonq@gimmungyuui-MacBookPro.local>
2024-11-25 22:36:08 +0900

    test: fix warning - globals setting

--- test/jest.unit.ts
+++ test/jest.unit.ts
@@ -2,13 +2,8 @@ export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
   testRegex: '\\.spec\\.ts$', // .spec.ts 파일만 테스트
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': ['ts-jest', { isolatedModules: true }],
   },
   rootDir: '../', // 루트 디렉토리 지정
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
 };
